eb5e6ef73671887f986fcfcbaf56dcf91ba082a3
ammargomaa1 <ammargomaa1@gmail.com>
2022-07-24 06:11:07 +0200

    Added Add to cart

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\MeController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Cart\CartController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,8 +12,10 @@ Route::resource('categories',\App\Http\Controllers\Categories\CategoryController
 Route::resource('products',\App\Http\Controllers\Products\ProductController::class);
 
 Route::group(['prefix'=>'auth'],function(){
-   Route::post('register',[\App\Http\Controllers\Auth\RegisterController::class,'action']);
-   Route::post('login',[\App\Http\Controllers\Auth\LoginController::class,'action']);
-   Route::get('me',[\App\Http\Controllers\Auth\MeController::class,'action']);
+   Route::post('register',[RegisterController::class,'action']);
+   Route::post('login',[LoginController::class,'action']);
+   Route::get('me',[MeController::class,'action']);
 
 });
+
+Route::resource('cart',CartController::class);
--- tests/Unit/Models/Users/UserTest.php
+++ tests/Unit/Models/Users/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Models\Users;
 
+use App\Models\ProductVariation;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -21,5 +22,29 @@ class UserTest extends TestCase
         ]);
 
         $this->assertNotEquals('cats',$user->password);
+    }
+
+    public function test_it_has_many_cart_products()
+    {
+        $user = User::factory()->create();
+
+        $user->cart()->attach(
+            ProductVariation::factory()->create()
+        );
+
+        $this->assertInstanceOf(ProductVariation::class,$user->cart->first());
+    }
+
+    public function test_it_has_quantity_foreach_cart_product()
+    {
+        $user = User::factory()->create();
+        
+        $user->cart()->attach(
+            ProductVariation::factory()->create(),[
+                'quantity' => $quantity = 5
+            ]
+        );
+
+        $this->assertEquals($user->cart->first()->pivot->quantity,$quantity);
     }
 }
